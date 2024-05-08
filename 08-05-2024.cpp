f61bc70471b5a2e0657339f63f07c138c628ec70
Drishti Jain <114685748+drishtij3@users.noreply.github.com>
2024-05-08 21:09:59 +0530

    Create 08-05-2024.cpp
    Root to Leaf Paths
    
    Given a Binary Tree of nodes, you need to find all the possible paths from the root node to all the leaf nodes of the binary tree.

---
+++ 08-05-2024.cpp
@@ -0,0 +1,41 @@
+
+Definition for Binary Tree Node
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+      void fun(Node* root,vector<int> temp,vector<vector<int>> &ans){
+        if(root==NULL)
+            return;
+        
+        temp.push_back(root->data);
+        
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(temp);
+            return;
+        }
+        
+        fun(root->left,temp,ans);
+        fun(root->right,temp,ans);
+        return;
+    }
+     
+    vector<vector<int>> Paths(Node* root) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        fun(root,temp,ans);
+        
+        return(ans);
+    }
+};
