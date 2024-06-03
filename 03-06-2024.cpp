435bf05f176aa823225fd48cc72224f16e7544d5
Drishti Jain <114685748+drishtij3@users.noreply.github.com>
2024-06-03 17:16:48 +0530

    Create 03-06-2024.cpp
    Trail of ones

---
+++ 03-06-2024.cpp
@@ -0,0 +1,14 @@
+ int numberOfConsecutiveOnes(int n) {
+        // code here
+        if(n==2)
+            return 1;
+        int a=0, b=1, c, mod=1e9+7, ans=1;
+        for(int i=3; i<=n; i++)
+        {
+            int c=(a+b)%mod;
+            a=b;
+            b=c;
+            ans=((ans*2)%mod+c)%mod;
+        }
+        return ans;
+    }
